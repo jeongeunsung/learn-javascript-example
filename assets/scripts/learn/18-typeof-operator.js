dff8adf4ee9c45885195ef2ed0c1f4cb8bc3452b
jeongeunsung <sje8402@gmail.com>
2025-06-13 09:27:31 +0900

    [정은] typeof 수업

--- assets/scripts/learn/18-typeof-operator.js
+++ assets/scripts/learn/18-typeof-operator.js
@@ -3,6 +3,7 @@
 // --------------------------------------------------------------------------
 
 const o = {
+  // key(string, number, symbol이 들어 갈수 있음 string에는 '' 이렇게 작은 따옴표로 할수 있고 name라고만 할 수 있음): value
   name: 'object',
   serialNumber: 20250613,
   isObjectType: () => true,
@@ -11,3 +12,13 @@ const o = {
   symbol: Symbol('o'),
   int: 100n,
 }
+
+console.log(typeof o)
+console.log(typeof o.name)
+console.log(typeof o.serialNumber)
+console.log(typeof o.isObjectType)
+console.log(typeof o.isObjectType())
+console.log(typeof o.isNotDefined)
+console.log(typeof o.empty)
+console.log(typeof o.symbol)
+console.log(typeof o.int)
